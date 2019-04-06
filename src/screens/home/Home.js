ef8df07f839e64771d0096f18ebb62852efd3a98
BradPrit09 <pritamchandra93@gmail.com>
2019-04-07 01:24:47 +0530

    Basic Home Done

---
+++ src/screens/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+import Header from '../../common/header/Header';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <Header />
+                HURRAY
+            </div>
+        ) }
+
+}
+export default Home;
